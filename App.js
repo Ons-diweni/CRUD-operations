8d9917a18f67f23b7b4abac23118b599eec282b0
Ons <ons.diweni@esprit.tn>
2023-03-07 18:02:31 +0100

    ODM - database connection

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 //importer le module express (c'est un Framework pour mettre en place des applications web basèe sur nodeJs)
 const express = require('express')
-
+const mongoose = require ('mongoose')
+const dbConfig = require ('./dbConfig/dbConfig.json')
 
 //création d'une instance d'une application Express en appelant la fonction factory express().
 //Ensuite, vous pouvez utiliser l'objet app pour configurer et gérer l'application Express
 var app = express ()
 
 
+//Connection à la base de données 
+mongoose.connect(dbConfig.dbURL , {useNewUrlParser: true,useUnifiedTopology: true})
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch((erreur) => console.log('Connexion à MongoDB échouée !', +erreur))
 
 
-//exportation de l'instance app pourqu'elle soit visible et accessible par tous les modules dans ce projet
+
+
+
+
+
+
+
+
+//exportation de l'instance app pour qu'elle soit visible et accessible par tous les modules dans ce projet
 module.exports=app ; 
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const http = require('http')
-const server = http.createServer()
-server.listen(3002 , ()=> console.log("hey i'm runnig") )
+const app = require('./App')
+
+
+const server = http.createServer(app)
+server.listen(3002 , ()=> console.log("server running") )
 
 
