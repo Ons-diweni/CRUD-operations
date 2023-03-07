2d0441b171bd089c0af4ccd99d3f816401886e97
Ons <ons.diweni@esprit.tn>
2023-03-07 04:44:54 +0100

    Clean Architecture from scratch

---
+++ App.js
@@ -0,0 +1,13 @@
+//importer le module express (c'est un Framework pour mettre en place des applications web basèe sur nodeJs)
+const express = require('express')
+
+
+//création d'une instance d'une application Express en appelant la fonction factory express().
+//Ensuite, vous pouvez utiliser l'objet app pour configurer et gérer l'application Express
+app = express ()
+
+
+
+
+//exportation de l'instance app pourqu'elle soit visible et accessible par tous les modules dans ce projet
+module.exports=app
---
+++ server.js
@@ -0,0 +1,5 @@
+const http = require('http')
+const server = http.createServer(()=> console.log("hey i'm runnig"))
+server.listen(3002)
+
+
